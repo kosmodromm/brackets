8b3992f820be34fadc756109e2b56c3f662b4ef0
Anton Kizerev <70804175+kosmodromm@users.noreply.github.com>
2020-10-20 15:08:17 +0300

    add: solution function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let stack = [];
+    if (str.length % 2 !== 0) {
+        return false;
+    } // проверяю скобки в строке на четность, что бы исключить строку без пары по количеству
+
+    for (j = 0; j < str.length; j++) {
+        for (i = 0; i < bracketsConfig.length; i++) { // проверяю совпадение конфига проходя по строке
+            if (bracketsConfig[i][0] === str[j] && stack[stack.length - 1] !== bracketsConfig[i][1]) { //кладу открывающие скобки в стэк, если последний элемент в нем не закрывающая скобка
+                stack.push(str[j]);
+            } else if (bracketsConfig[i][1] === str[j]) {
+                if (stack[stack.length - 1] === bracketsConfig[i][0]) { // иначе, при закрытой паре, удаляю открывающую часть из стэка
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return stack.length === 0 ? true : false;
 }
